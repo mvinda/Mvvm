14858535191754d5861d482c466c3cff9f9a88db
weihuada <weihuada@cvte.com>
2021-04-29 21:49:55 +0800

    update .gitignore

--- common/build/generated/source/apt/debug/com/example/common/BR.java
+++
@@ -1,7 +0,0 @@
-package com.example.common;
-
-public class BR {
-  public static int _all = 0;
-
-  public static int viewModel = 1;
-}
--- common/build/generated/source/dataBinding/trigger/debug/com/example/common/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.example.common;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
